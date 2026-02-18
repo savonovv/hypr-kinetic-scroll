12fcd36567da57eced0514f8c17b801f8d101bd8
savonovv <savonovv@link.it>
2026-02-18 19:17:56 +0400

    Stop inertia when scroll target changes

--- kinetic.hpp
+++ kinetic.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <hyprland/src/devices/IPointer.hpp>
 #include <wayland-server-core.h>
+#include <cstdint>
 
 class KineticState {
   public:
@@ -15,11 +16,13 @@ class KineticState {
     static int onDecayTimer(void* data);
     void       emitSyntheticScroll();
 
-    double   m_velocityV    = 0.0;
-    double   m_velocityH    = 0.0;
-    uint32_t m_lastEventMs  = 0;
-    bool     m_tracking     = false;
-    bool     m_decaying     = false;
+    double    m_velocityV             = 0.0;
+    double    m_velocityH             = 0.0;
+    uint32_t  m_lastEventMs           = 0;
+    bool      m_tracking              = false;
+    bool      m_decaying              = false;
+    uintptr_t m_scrollTargetWindowKey = 0;
+    uintptr_t m_scrollTargetSurfaceKey = 0;
 
     wl_event_source* m_stopTimer  = nullptr;
     wl_event_source* m_decayTimer = nullptr;
